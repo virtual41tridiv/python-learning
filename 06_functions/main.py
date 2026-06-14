1e192da99795bd6e02e5f7cb5e6487459868f57c
TRIDIV SWAIN <tridivswain.india2@gmail.com>
2026-06-14 17:40:40 +0800

    Update Functions

---
+++ 06_functions/main.py
@@ -0,0 +1,4 @@
+import myutils
+
+print(myutils.find_triangle_area(10, 5))
+print(myutils.find_volume_cyclinder(3, 10))
---
+++ 06_functions/myutils.py
@@ -0,0 +1,14 @@
+def find_triangle_area(base, height):
+    print("base:", base)
+    print("height:", height)
+    area = 0.5 * base * height
+    return area
+
+
+
+def find_volume_cyclinder(radius, height):
+    print("radius:", radius)
+    print("height:", height)
+    pi =3.14
+    volume = pi * radius**2 * height
+    return volume
